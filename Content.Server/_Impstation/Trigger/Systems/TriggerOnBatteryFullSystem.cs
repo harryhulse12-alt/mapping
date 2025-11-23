37ee9f33751bc36548cee601e17bc4e9ed2e4b6d
harryhulse12-alt <harryhulse12@gmail.com>
2025-11-23 20:45:33 +0000

    draft PR

--- Content.Server/_Impstation/Trigger/Systems/TriggerOnBatteryFullSystem.cs
+++ Content.Server/_Impstation/Trigger/Systems/TriggerOnBatteryFullSystem.cs
@@ -1,16 +1,16 @@
 using Content.Server._Impstation.Trigger.Components.Triggers;
 using Content.Server.Power.Components;
 using Content.Server.Power.EntitySystems;
-using Content.Shared.Trigger.Systems;
 using Content.Shared.Power;
 using Content.Shared.Power.Components;
+using Content.Server.Explosion.EntitySystems;
 
 namespace Content.Server._Impstation.Trigger.Systems;
 
 public sealed class TriggerOnBatteryFullSystem : EntitySystem
 {
-    [Dependency] private readonly TriggerSystem _trigger = default!;
     [Dependency] private readonly BatterySystem _battery = default!;
+    [Dependency] private readonly TriggerSystem _trigger = default!;
 
     public override void Initialize()
     {
@@ -19,7 +19,7 @@ public sealed class TriggerOnBatteryFullSystem : EntitySystem
         SubscribeLocalEvent<TriggerOnBatteryFullComponent, ChargeChangedEvent>(OnChargeChanged);
     }
 
-    private void OnChargeChanged(Entity<TriggerOnBatteryFullComponent> ent, ref ChargeChangedEvent args)
+    private void OnChargeChanged(Entity<PowerCellComponent> ent, ref ChargeChangedEvent args)
     {
         if (TryComp(ent.Owner, out BatteryComponent? battery) && _battery.IsFull(ent.Owner, battery))
         {
